f4b260b31947a968a723407948172bc6111f5f19
Lady Clu <celupanow@gmail.com>
2021-02-22 13:33:56 -0700

    adding comments

--- assets/javascript/submit.js
+++ assets/javascript/submit.js
@@ -1,10 +1,15 @@
 
+// on click function for the submit button
 $("#submit").on("click", function(event) {
     event.preventDefault();
 
+    // taking in the value put in the ssn and dob inputs
     var ssn = $("#ssn-input").val().trim();
     var dob = $("#dob-input").val().trim();
 
+    // if the ssn is empty, an alert asks the user to enter an ssn
+    // if the dob is empty, an alert asks for the user to enter a dob
+    // if neither are empty, an alert saying thank you pops up
     if (ssn == "") {
         alert("Please enter a valid ssn");
     } else if (dob == "") {
@@ -12,5 +17,4 @@ $("#submit").on("click", function(event) {
     } else {
         alert("Thank you for your submission");
     }
-
 });
